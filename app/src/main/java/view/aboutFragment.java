978c47d7436770f5b2c4aecef45c62e942f226d4
YuanJustin <ywz1994@163.com>
2017-06-11 00:57:07 +0300

    除了菜单外都完成了

--- app/src/main/java/view/aboutFragment.java
+++ app/src/main/java/view/aboutFragment.java
@@ -3,17 +3,15 @@ package view;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.rui.myapplication.MainActivity;
-import com.example.rui.myapplication.R;
+import com.Justin.Yuan.ClinicalSkillApp.MainActivity;
+import com.Justin.Yuan.ClinicalSkillApp.R;
 
 import moe.feng.alipay.zerosdk.AlipayZeroSdk;
 
