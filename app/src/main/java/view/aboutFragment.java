6a6ba02443cdc179579f947ff02b89f520619fa1
YuanJustin <ywz1994@163.com>
2017-06-10 23:22:49 +0300

    完全了

--- app/src/main/java/view/aboutFragment.java
+++ app/src/main/java/view/aboutFragment.java
@@ -3,19 +3,28 @@ package view;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.rui.myapplication.MainActivity;
 import com.example.rui.myapplication.R;
+
+import moe.feng.alipay.zerosdk.AlipayZeroSdk;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class aboutFragment extends Fragment {
 
-
-    public aboutFragment() {
+MainActivity mainActivity;
+    public aboutFragment(MainActivity mainActivity) {
+        this.mainActivity=mainActivity;
         // Required empty public constructor
     }
 
@@ -24,6 +33,32 @@ public class aboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View v= inflater.inflate(R.layout.fragment_about, container, false);
+        TextView t=(TextView)v.findViewById(R.id.textView2);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(AlipayZeroSdk.hasInstalledAlipayClient(mainActivity)){
+                    AlipayZeroSdk.startAlipayClient(mainActivity,"a6x07259dtyuujb9kg4mk09");
+                }else{
+                    Toast.makeText( mainActivity,"您未安装支付宝", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        ImageView image=(ImageView)v.findViewById(R.id.imageView2);
+
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(AlipayZeroSdk.hasInstalledAlipayClient(mainActivity)){
+                    AlipayZeroSdk.startAlipayClient(mainActivity,"a6x07259dtyuujb9kg4mk09");
+                }
+                else{
+                    Toast.makeText( mainActivity,"您未安装支付宝", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        return v;
+
     }
 }
